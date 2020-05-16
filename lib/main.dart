64bf421f028bddb5ead70f8232db2e80fe36e7f1
atharva_dawande <atharva.dawande603@gmail.com>
2020-05-16 13:50:22 +0530

    used random to generate random output in images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text(
+              'Ask me anything',
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.white,
+          ),
+          body: Ballpage(),
+        ),
       ),
     );
+
+class Ballpage extends StatefulWidget {
+  @override
+  _BallpageState createState() => _BallpageState();
+}
+
+class _BallpageState extends State<Ballpage> {
+  var randomnum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Expanded(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              randomnum = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$randomnum.png'),
+        ),
+      )
+    ]);
+  }
+}
